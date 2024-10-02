d24c35b0f8406d7dda7818c7445783210e3ae457
junny1117 <junny1117@naver.com>
2024-10-02 15:27:00 +0900

    추가

--- project4-2/src/App.js
+++ project4-2/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Menu from './components/Menu';
+import Contents from './components/Contents';
+import BookReview from './components/BookReview';
+import Image from './components/Image';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu />
+      <Contents />
+      <BookReview />
+      <Image />
     </div>
   );
 }

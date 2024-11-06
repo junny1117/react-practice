12eb0511d2bd6c9a1486213e9121de0d58dc6aaf
junny1117 <junny1117@naver.com>
2024-11-06 15:12:29 +0900

    -

---
+++ project12-02/src/jsx/styles.js
@@ -0,0 +1,13 @@
+const styles = {
+    wrapper: {
+        padding: 16,
+        display: 'flex',
+        flexDirection: 'row',
+        borderBottom: '1px solid grey',
+    },
+    greeting: {
+        marginRight: 8,
+    },
+};
+
+export default styles;
